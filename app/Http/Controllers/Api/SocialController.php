895dc9da9d60ca5592bcf4fbbe9206811d34611a
luke <luka9001@hotmail.com>
2020-01-23 12:06:38 +0800

    2020012300

--- app/Http/Controllers/Api/SocialController.php
+++ app/Http/Controllers/Api/SocialController.php
@@ -133,10 +133,14 @@ class SocialController extends Controller
     //获取我发布的社交圈信息
     public function getMySocial(Request $request)
     {
+        $currentUserId = $request->user()->id;
         $id = request('id');
         if ($id != null) {
             $socialMessage = SocialMessage::leftJoin('users', 'social_message.user_id', '=', 'users.id')->where('social_message.user_id', $id)->select('social_message.id', 'social_message.user_id', 'social_message.message', 'social_message.liked', 'social_message.created_at', 'social_message.photos', 'users.lifephoto', 'users.name', 'users.live', 'users.sex')->orderBy('id', 'desc')->paginate(10);
             foreach ($socialMessage as $item) {
+                if ($item['user_id'] === $currentUserId) {
+                    $item['is_current_user'] = 1;
+                }
                 $item['likescount'] = Likes::where('social_message_id', '=', $item->id)->count();
                 $item['commentcount'] = Comment::where('social_message_id', '=', $item->id)->count();
             }
